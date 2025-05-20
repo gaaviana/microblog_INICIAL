6331d43f8976e4d31d589a206f3f2fa9022544f1
Gabriel Viana <gaa.vianaa@gmail.com>
2025-05-20 09:34:05 -0300

    textinho

---
+++ src/Auth/ControleDeAcesso.php
@@ -0,0 +1,10 @@
+<?php 
+namespace Microblog\Auth;
+
+/* Sobre sessões no php
+Sessões (SESSION) é uma funcionalidade usada para controle de acesso e outras informações que sejam importantes enquanto o navegador estiver aberto com o site.
+
+exemplos: áreas administrativas, painel de controle/dashboard, aréa do Cliente, aréa do aluno etc.
+
+Nestas áreas o acesso se dá através de alguma forma de atenticação. Exemplos: login/senha, biometria, facial, 2-fatores etc.
+*/

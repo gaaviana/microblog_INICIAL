0b972dc994e20d882938b3ce472ef36746de99d7
Gabriel Viana <gaa.vianaa@gmail.com>
2025-05-20 09:52:25 -0300

    iniciando uma sessão

--- src/Auth/ControleDeAcesso.php
+++ src/Auth/ControleDeAcesso.php
@@ -8,3 +8,27 @@ exemplos: áreas administrativas, painel de controle/dashboard, aréa do Cliente
 
 Nestas áreas o acesso se dá através de alguma forma de atenticação. Exemplos: login/senha, biometria, facial, 2-fatores etc.
 */
+
+final class ControleDeAcesso {
+
+    private function __construct() { }
+
+    /* Inicia uma sessõa caso não tenha nenhuma em andamento */
+    private static function iniciarSessao():void {
+        if(!isset($_SESSION)) session_start();
+    }
+
+    /* bloqueia paginas adm caso o usuario não esteja logado */
+    public function exigirLogin() : void {
+
+        /* inicia sessao (se necessário) */
+        self::iniciarSessao();
+
+        /* se NÃO EXISTIR uma variável de sessão chamada ID */
+        if (!isset($_SESSION['id'])) {
+            session_destroy();
+            header("location:../login.php?acesso_proibido");
+            exit;
+        }
+    }
+}
